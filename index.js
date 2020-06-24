0d271e9d32f8a958c067f430de5b432d49a93cfd
Abhishek Sirohi <abhishek@totalcloud.io>
2020-06-24 09:50:24 +0530

    Port fix

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require("path");
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use(express.json({ extended: false }));
 
 app.use(cors());
@@ -16,8 +18,8 @@ app.use("/api", apiRouter);
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.listen(5000, () => {
-  console.log(`Listening on PORT 5000`);
+app.listen(port, () => {
+  console.log(`Listening on PORT ${port}`);
 });
 
 module.exports = app;
